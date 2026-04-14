d6253a281dc9d8c13525db84e2638da22fb58fd5
Denver Cowan <denvercowan1@gmail.com>
2026-04-14 16:08:20 -0400

    repo setup commit

---
+++ start.py
@@ -0,0 +1,17 @@
+from turtle import *
+bgcolor('black')
+color('blue')
+fillcolor('pink')
+begin_fill()
+
+# drawing the shape
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+
+end_fill()
+mainloop()
