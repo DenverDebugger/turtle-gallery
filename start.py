15fa2aa716d69573652f3b44922d62eb89b5ab0c
Denver Cowan <denvercowan1@gmail.com>
2026-04-16 16:52:13 -0400

    added stubs for future shapes

--- start.py
+++ start.py
@@ -11,6 +11,15 @@ class MyTurtle(Turtle):
         for _ in range(3):
             self.fd(size)
             self.lt(120)
+    
+    # TODO: Hexagon
+    # TODO: Octagon
+    # TODO: Circle
+    # TODO: Spiral
+    # TODO: Star
+    # TODO: Mosaic
+    # TODO: Fractals
+    # TODO: Regular Polyhedra
 
 t = MyTurtle()
 t.square(100)
